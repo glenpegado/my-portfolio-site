03823aad94cc11a035bc091513df4a6ccf8d6d7e
Haur Kang <haur.kang@gmail.com>
2019-06-11 22:32:15 +0100

    Complete landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,7 +10,19 @@ class LandingPage extends Component {
           <Cell col={12}>
             <img src={mypic} alt={mypic} className="avatar-img"/>
             <div className="banner-text"> 
-              <h1>Front-end React developer</h1>
+              <h1>Front-end React Developer</h1>
+              <hr/>
+              <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+                <div className="social-links">
+                  {/* LinkedIn */}
+                  <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                  </a>
+                  {/* Github */}
+                  <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-github-square" aria-hidden="true" />
+                  </a>
+                </div>
             </div>
           </Cell>
         </Grid>
