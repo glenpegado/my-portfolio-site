37192e386a87aee0d6336cdb5cac9da8be061f44
Haur Kang <haur.kang@gmail.com>
2019-06-11 21:41:18 +0100

    Add mypic

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Cell } from 'react-mdl';
+import mypic from '../mypic.jpg';
 
 class LandingPage extends Component {
   render() {
@@ -7,6 +8,7 @@ class LandingPage extends Component {
       <div style={{width:'100%', margin:'auto'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
+            <img src={mypic} alt={mypic} className="avatar-img"/>
           </Cell>
         </Grid>
       </div>
